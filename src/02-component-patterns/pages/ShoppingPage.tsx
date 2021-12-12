32fe84d8fd3aa7e77b6dd18d54b4942443223ab5
Ricardo Vargas <ricardo.vargasdev@gmail.com>
2021-12-12 12:59:05 -0600

    Basis of the project

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,12 @@
+import ProductCard from '../components/ProductCard';
+
+const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>Shopping store</h1>
+      <ProductCard />
+    </div>
+  );
+};
+
+export default ShoppingPage;
