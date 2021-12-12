211ec4e3a3908ce50082cfae574332c04a028776
Ricardo Vargas <ricardo.vargasdev@gmail.com>
2021-12-12 16:20:28 -0600

    Firsts steps Compound Component Pattern

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,22 +1,44 @@
 import styles from '../assets/styles/styles.module.css';
+import { useProduct } from '../hooks/useProduct';
+
 import noImage from '../assets/no-image.jpg';
-import { useState } from 'react';
 
-const ProductCard = () => {
-  const [counter, setCounter] = useState(0);
+interface Props {
+  product: Product;
+}
 
-  const increaseBy = (value: number) => {
-    setCounter((prev) => Math.max(prev + value, 0));
-  };
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+// img is opcional
+export const ProductImage = ({ img = '' }) => {
+  return (
+    <img
+      className={styles.productImg}
+      src={img ? img : noImage}
+      alt="Product image"
+    />
+  );
+};
+
+// title is required
+export const ProductTitle = ({ title }: { title: string }) => {
+  return <span className={styles.productDescription}>{title}</span>;
+};
+
+// Al hacerlo de esta forma, obligamos a que mande un producto siempre
+
+const ProductCard = ({ product }: Props) => {
+  const { counter, increaseBy } = useProduct();
 
   return (
     <div className={styles.productCard}>
-      <img
-        className={styles.productImg}
-        src="./coffee-mug.png"
-        alt="Coffe Mug"
-      />
-      <span className={styles.productDescription}>Coffe Mug</span>
+      <ProductImage img={product.img} />
+
+      <ProductTitle title={product.title} />
 
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,10 +1,25 @@
 import ProductCard from '../components/ProductCard';
 
+const product = {
+  id: '1',
+  title: 'Cherry Cake',
+  img: 'https://images.unsplash.com/photo-1625579437459-12c5f5762142?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+};
+
 const ShoppingPage = () => {
   return (
     <div>
       <h1>Shopping store</h1>
-      <ProductCard />
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+      >
+        <ProductCard product={product} />
+      </div>
     </div>
   );
 };
